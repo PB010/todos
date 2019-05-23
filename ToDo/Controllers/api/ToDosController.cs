26de6411cf7eb56937813c2a1ad224581d499a08
Petru Betco <petrubetco@gmail.com>
2019-05-23 16:25:19 +0300

    Added status toggle button

--- ToDo/Controllers/api/ToDosController.cs
+++ ToDo/Controllers/api/ToDosController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Data.Entity;
 using System.Linq;
 using System.Web.Http;
+using ToDo.Core.Models;
 using ToDo.Persistence;
 
 namespace ToDo.Controllers.api
@@ -19,6 +20,35 @@ namespace ToDo.Controllers.api
         public IHttpActionResult Get()
         {
             var todo = _context.ToDoLists.Include(t => t.ToDoPriorities).ToList();
+
+            return Ok(todo);
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            var todo = _context.ToDoLists.Single(t => t.Id == id);
+
+            if (todo == null)
+                return NotFound();
+
+            _context.ToDoLists.Remove(todo);
+            _context.SaveChanges();
+
+            return Ok(todo);
+        }
+
+        [HttpPut]
+        public IHttpActionResult ChangeStatus(int id)
+        {
+            var todo = _context.ToDoLists.Single(t => t.Id == id);
+
+            if (todo == null)
+                return BadRequest("There was no such todo.");
+
+            todo.ToDoStatus = todo.ToDoStatus == ToDoStatus.Open ? ToDoStatus.Done : ToDoStatus.Open;
+
+            _context.SaveChanges();
 
             return Ok(todo);
         }
