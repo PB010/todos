1ffd7c60129ffa8fe972d8cb06424d203b48a964
Petru Betco <petrubetco@gmail.com>
2019-05-25 17:24:35 +0300

    Refactored email sender

---
+++ ToDo/Persistence/Email/EmailSender.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using ToDo.Core.ViewModels;
+
+namespace ToDo.Persistence.Email
+{
+    public class EmailSender
+    {
+        private static readonly ApplicationDbContext _context;
+
+        static EmailSender()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public static void SendAnHourBefore(ToDoViewModel toDo, string userId, TimeSpan time)
+        {
+            var emailCheck = _context.EmailHourChecks
+                .Single(e => e.ToDoListId == toDo.Id && e.ApplicationUserId == userId);
+
+            if (time.TotalMinutes <= 60 && time.TotalMinutes > 30 && !emailCheck.Hour)
+            {
+                EmailTemplate.Template("Expiring ToDo - 1 hour or less.",
+                    toDo,
+                    emailCheck.Hour,
+                    _context);
+            }
+            
+        }
+
+        public static void SendHalfAnHourBefore(ToDoViewModel toDo, string userId, TimeSpan time)
+        {
+            var emailCheck = _context.EmailHourChecks
+                .Single(e => e.ToDoListId == toDo.Id && e.ApplicationUserId == userId);
+
+            if (time.TotalMinutes <= 30 && time.TotalMinutes > 0 && !emailCheck.HalfAnHour)
+            {
+                EmailTemplate.Template("Expiring ToDo - 30 minutes or less.",
+                    toDo,
+                    emailCheck.HalfAnHour,
+                    _context);
+            }
+
+                
+        }
+    }
+
+}
